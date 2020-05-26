fe561ea9d0d1ad327dd1bfa6079b7a1a5e304968
qiangvei <qiangvei@hotmail.com>
2020-05-27 01:06:46 +0800

    fba

--- NetBarcodeDemo/Form1.cs
+++ NetBarcodeDemo/Form1.cs
@@ -20,13 +20,15 @@ namespace NetBarcodeDemo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String data = txtStr.Text.Trim();
+            String data = txtStr.Text.Trim().ToUpper();
             
-            int ww = picBarCodeImg.Width;
-            int hh = picBarCodeImg.Height;
+            int ww = picBarCodeImg.Width/4;
+            int hh = picBarCodeImg.Height/4;
             string otherText = "some text will show here draw datastring under the barcode image some text will show here draw datastring under the barcode image";
-            BarcodeForImg nb = new BarcodeForImg(data, otherText, NetBarcode.Type.Code128A, true, ww, hh, new Font("Consolas", 12));
-            picBarCodeImg.Image = nb.GetImage();
+            //BarcodeForImg nb = new BarcodeForImg(data, otherText, NetBarcode.Type.Code128A, true, ww, hh, new Font("Consolas", 12));
+            //picBarCodeImg.Image = nb.GetImage(); 
+            FbaLable itlab = new FbaLable(data, "some text here", NetBarcode.Type.Code128A);
+            picBarCodeImg.Image = itlab.GetItemLable();
         }
     }
 }
